99c6bd1bafd818656755790bfbf03797453a0fe2
iamsilva07 <iamsilvavazquez@gmail.com>
2026-04-07 20:35:04 +0200

    CORS configurado para Flutter
    CORS configurado para Flutter

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import Base, engine
 from app.models import user, document
 from app.api.routes import auth, documents
@@ -9,6 +10,14 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="TFC API",
     version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(auth.router, prefix="/api/v1")
