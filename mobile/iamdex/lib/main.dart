9465e18bc7e73531b93cf7823d9b0b271fc3c9f2
iamsilva07 <iamsilvavazquez@gmail.com>
2026-05-21 19:33:33 +0200

    Modo oscuro automático según tema del sistema
    Modo oscuro automático según tema del sistema

--- mobile/iamdex/lib/main.dart
+++ mobile/iamdex/lib/main.dart
@@ -28,6 +28,14 @@ class MyApp extends StatelessWidget{
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
       home: !onboardingDone
           ? const OnboardingScreen()
           : isLoggedIn
